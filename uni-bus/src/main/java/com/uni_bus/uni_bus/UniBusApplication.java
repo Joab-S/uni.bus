d2313ad6bd3ba8f7ec741363f6e11f4b483dbe52
Joab-S <joabsilva2002@gmail.com>
2024-10-08 20:22:27 -0300

    Project created

---
+++ uni-bus/src/main/java/com/uni_bus/uni_bus/UniBusApplication.java
@@ -0,0 +1,13 @@
+package com.uni_bus.uni_bus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniBusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniBusApplication.class, args);
+	}
+
+}
